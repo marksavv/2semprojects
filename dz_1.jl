835f78342a6f4e704f2d2da0abe39551ed6d0a73
marksavv <70964357+marksavv@users.noreply.github.com>
2021-05-05 21:42:37 +0400

    Practise_2_task1.jl

---
+++ dz_1.jl
@@ -0,0 +1,13 @@
+function sortkey(x,a)
+    size=length(x)
+    for i in 1:size
+        for j in 1:size
+            if x[i]>x[j]
+                x[i],x[j]=x[j],x[i]
+                a[i],a[j]=a[j],a[i]
+            end
+        end
+    end
+    return a
+end
+
