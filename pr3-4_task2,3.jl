2e2891a2734b6a5d684106d1e8a13f9670fb5ee9
marksavv <70964357+marksavv@users.noreply.github.com>
2021-05-18 09:00:44 +0300

    Update pr3-4_task2,3.jl

--- pr3-4_task2,3.jl
+++ pr3-4_task2,3.jl
@@ -19,10 +19,12 @@ function new_buble_sort2(a::Vector{Int})    #ShakerSort
     left=2
     right=length(a)
     while (left<=right)
-        for i in right:left
-            if (a[i-1]>a[i])
-                a[i-1],a[i]=a[i],a[i-1]
+        j=right
+        while(j>2)
+            if (a[j-1]>a[j])
+                a[j-1],a[j]=a[j],a[j-1]
             end
+            j-=1
         end
         for i in left:right
             if (a[i-1]>a[i])
