459dbaef9eca6a2ce76d65043f510787c2b9de80
marksavv <70964357+marksavv@users.noreply.github.com>
2021-06-05 11:16:16 +0300

    Add files via upload

---
+++ pr5_taskk2.jl
@@ -0,0 +1,13 @@
+function evaldiffpoly3(x,A)
+    Q′′′=0
+    Q′′=0
+    Q′=0
+    Q=0
+    for a in A
+        Q′′′=Q′′′*x+Q′′
+        Q′′=Q′′*x+Q′
+        Q′=Q′*x+Q
+        Q=Q*x+a
+    end
+    return Q, Q′,2*Q′′,6*Q′′′
+end
