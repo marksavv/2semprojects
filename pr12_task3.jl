fe8224d226eeeeccf77ccd0a18bf155da9b2c0c8
marksavv <70964357+marksavv@users.noreply.github.com>
2021-06-13 12:40:27 +0300

    Add files via upload

---
+++ pr12_task3.jl
@@ -0,0 +1,9 @@
+points=[s for s in zip([1.0,0.0,5.0,5.5,19.0,7.0,3.0,4.5],[0.0,6.0,7.0,5.0,-5.0,5.5,4.0,1.0])]
+
+function plosh_trap(points)
+    s=0
+    for i in 2:length(points)
+        s=s+1/2*(points[i-1][2]+points[i][2])*(points[i][1]-points[i-1][1])
+    end
+    return s
+end
