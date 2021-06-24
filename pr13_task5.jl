7694384d61dc01681ab9fa3589bbf19a861d5cb3
marksavv <70964357+marksavv@users.noreply.github.com>
2021-06-24 12:13:06 +0300

    Update pr13_task5.jl

--- pr13_task5.jl
+++ pr13_task5.jl
@@ -20,19 +20,19 @@ end
 NumSplit{N}() where N = NumSplit{N}(collect(1:N), N)
 
 function next!(split::NumSplit{T}) where T 
-if split.num_terms == 1
-    false
-end
-s=split.value
-k=split.num_terms
-i=k-1
-while i > 1 && s[i-1]==s[i]
-    i -= 1
-end
-s[i] += 1
-r=sum(@view s[i+1:k])
-k=i+r-1
-s[i+1:k] .= 1
-split.num_terms = k
-return true
+    if split.num_terms == 1
+        false
+    end
+    s=split.value
+    k=split.num_terms
+    i=k-1
+    while i > 1 && s[i-1]==s[i]
+        i -= 1
+    end
+    s[i] += 1
+    r=sum(@view s[i+1:k])
+    k=i+r-1
+    s[i+1:k] .= 1
+    split.num_terms = k
+    return true
 end
